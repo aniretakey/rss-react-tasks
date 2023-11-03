656570d2232b5dcd1ebc63d7e0d89b588fd44b6e
aniretakey <EMKornishina@gmail.com>
2023-11-03 21:04:28 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
-
+import { App } from './App';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>
 );
+
+//     children: [
+//       {
+//         path: '/search',
+//       }
+//     ]
