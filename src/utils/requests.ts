5ea0286d825e85c5e1f26f9ad750ec100ceadab2
aniretakey <EMKornishina@gmail.com>
2023-11-03 19:59:53 +0300

    feat: add request for get item by objectNum

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,13 +1,23 @@
 const key = import.meta.env.VITE_API_TOKEN;
 
+async function searchByCentury(
+  century = '1',
+  page?: 1,
+  itemsOnPage?: number
+): Promise<Response> {
+  const URL = `https://www.rijksmuseum.nl/api/en/collection/?key=${key}&f.dating.period=${+century}&ps=${itemsOnPage}&p=${page}`;
+  return await fetch(URL);
+}
+// TODO: MAX IMAGES COUNT = 10 000 (itemsOnPage * page<= 10 000)
+
 async function getCardsData(page: number): Promise<Response> {
   const itemsCount = 10;
-  const URL = `https://www.rijksmuseum.nl/api/en/collection/?key=${key}&imgonly=true&ps=${itemsCount}&p=${page}`;
+  const URL = `https://www.rijksmuseum.nl/api/en/collection/?key=${key}&ps=${itemsCount}&p=${page}`;
   return await fetch(URL);
 }
 
-async function searchByCentury(century = '18', page?: 1): Promise<Response> {
-  const URL = `https://www.rijksmuseum.nl/api/en/collection/?key=${key}&f.dating.period=${+century}&p=${page}`;
+async function getPictureByKey(key: string): Promise<Response> {
+  const URL = `https://www.rijksmuseum.nl/api/en/collection/${key}?key=OS3z3UmI`;
   return await fetch(URL);
 }
 
