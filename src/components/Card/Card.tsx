c83bfc62498eac8ceb9be57dfae8208775ea03d7
aniretakey <EMKornishina@gmail.com>
2023-11-03 13:09:44 +0300

    feat: converted Card into func comp

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,53 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './card.module.css';
-import { getPictures } from '../../utils/requests';
-import { ArtObject, CardsProps } from '../../types/types';
+import { CardsProps } from '../../types/types';
 import { Loader } from '../Loader/Loader';
 
-export default class Card extends React.Component<
-  CardsProps,
-  { pictures: ArtObject[]; isLoaded: boolean }
-> {
-  constructor(props: CardsProps) {
-    super(props);
-    this.state = {
-      pictures: [],
-      isLoaded: false,
-    };
-  }
+const Card = (pictures: CardsProps) => {
+  const [loading, setLoading] = useState(false);
 
-  static getDerivedStateFromProps() {
-    return null;
-  }
+  useEffect(() => {
+    setLoading(true);
+  }, []);
 
-  componentDidMount() {
-    this.setState({ isLoaded: false });
-    getPictures(1)
-      .then((response) => response.json())
-      .then(({ artObjects }) => {
-        this.setState({ pictures: artObjects });
-        this.setState({ isLoaded: true });
-      });
-  }
+  return (
+    <div className={styles.card}>
+      {loading ? (
+        <img
+          src={pictures.imgURL}
+          alt={pictures.title}
+          className={styles.cardImage}
+        />
+      ) : (
+        <Loader />
+      )}
+      <h1 className={styles.cardTitle}>{pictures.title}</h1>
+      <p className={styles.cardAuthor}>{pictures.author}</p>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className={styles.card}>
-        {this.state.isLoaded ? (
-          <img
-            src={this.props.imgURL}
-            alt={this.props.title}
-            className={styles.cardImage}
-            onLoad={() => {
-              this.setState({ isLoaded: true });
-            }}
-          />
-        ) : (
-          <Loader />
-        )}
-        <h1 className={styles.cardTitle}>{this.props.title}</h1>
-        <p className={styles.cardAuthor}>{this.props.author}</p>
-      </div>
-    );
-  }
-}
+export { Card };
